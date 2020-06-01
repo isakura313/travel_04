ad210c153a51cd07d90bab48d043598a3f5e8d36
Paul <isakura313@gmail.com>
2020-06-01 20:44:48 +0300

    asdasd

--- config/bd.config.php
+++ config/bd.config.php
@@ -1,8 +1,11 @@
 <?php
-$connect = new mysqli("localhost", "pavel",'', "travel");
+$connect = new mysqli("192.168.199.2", "root",'', "travel");
 $connect -> set_charset("utf8mb4");
 if($connect->connect_errno){
     die("Ошибка соединения: " . $connect->connect_errno);
 }
 
 ?>
+
+
+
